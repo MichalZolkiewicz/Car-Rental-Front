e9480978fd4043e0b395daa99081976ee7d0958a
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-04-27 19:50:02 +0200

    initial commit

---
+++ src/main/java/com/rental/car/front/RentalFrontApplication.java
@@ -0,0 +1,13 @@
+package com.rental.car.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rental/car/front/RentalFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.car.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
